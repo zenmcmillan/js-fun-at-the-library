e3618e1bbe06df2d1dd7504eee64ea104eaf72a9
Zen McMillan <zenmcmillan@gmail.com>
2023-07-09 10:48:11 -0600

    Solve unit tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,42 @@
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+   unshelfBook,
+   listTitles,
+   searchShelf
 };
+
+function shelfBook(bookObject, shelfArray) {
+  if (shelfArray.length < 3) {
+    shelfArray.unshift(bookObject)   
+  }  
+}
+
+function unshelfBook(bookTitle, booksArray) {
+ for (var i = 0; i < booksArray.length; i++) {
+  if (booksArray[i].title === bookTitle) {
+   booksArray.splice(i, 1)
+  }
+ }
+ return booksArray
+}
+
+function listTitles(booksArray) {
+  var bookTitles = []
+  for ( var i = 0; i < booksArray.length; i++) {
+    if (!booksArray[i].hasOwnProperty('title')) {
+      continue;
+    }
+    bookTitles.push(booksArray[i].title)
+  }
+  return bookTitles.join(', ')
+}
+
+function searchShelf(booksArray, bookTitle) {
+  for (var i = 0; i < booksArray.length; i++) {
+    if (booksArray[i].title === bookTitle) {
+      return true 
+    }
+  }
+  return false
+}
